8059c6d0b585d775522c2c38d1e6d588bb9b171d
Kaushiki Saxena <kaushiki1507@gmail.com>
2026-06-07 18:33:08 +0530

    Add comments for isAnagram function implementation
    explaining the code and adding time and space complexity remarks

--- Data Structures & Algorithms/is-anagram/submission-0.cpp
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -1,3 +1,4 @@
+// Time Complexity: O(n) Space Complexity: O(1)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -5,13 +6,14 @@ public:
         int ns = s.length();
         int nt = t.length();
         if(ns==nt)
+            //increments array alpha for particular letters in string s and then decrements for particular letters in string t
         { int alph[26]={0};
             for(int i=0;i<ns;i++)
             { 
                 alph[s[i]-'a']++;
                 alph[t[i]-'a']--;
             }
-
+//if absolute sum is 0, that is the letters for which arrray was incremented and decremented were the same, then it is an anagram
             for(int i=0;i<26;i++)
             {
                  sum+=abs(alph[i]);
