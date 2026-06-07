5a800c430f42d2277bcfcd3ff6e493b7c71e986a
Kaushiki Saxena <kaushiki1507@gmail.com>
2026-06-07 18:47:28 +0530

    Enhance comments for duplicate checking logic
    Added a comment to explain the duplicate checking process.

--- Data Structures & Algorithms/duplicate-integer/submission-1.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -1,7 +1,9 @@
+// Time Complexity: O(n^2) Space Complexity: O(1)
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
         int n = nums.size();
+        //iterates through array checking every pair for duplicacy 
         for(int i =0; i<n;i++)
         {for(int j = i+1;j<n;j++)
         {
