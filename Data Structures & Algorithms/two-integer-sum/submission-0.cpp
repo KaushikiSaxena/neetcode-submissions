07ed092b91d3620eead67241b6b2b61e372457c1
Kaushiki Saxena <kaushiki1507@gmail.com>
2026-06-07 18:25:46 +0530

    Enhance comments in twoSum function
    Added comments for time and space complexity, and clarified the inner loop's purpose.

--- Data Structures & Algorithms/two-integer-sum/submission-0.cpp
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -1,7 +1,9 @@
+// Time Complexity: O(n^2) Space Complexity: O(1)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n= nums.size();
+        //go through each combination and check conditions, if true, return values
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target && i != j)
