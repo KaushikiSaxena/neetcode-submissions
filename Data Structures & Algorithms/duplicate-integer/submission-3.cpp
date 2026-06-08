85a1472ca94083f67a56d63c4a9f3892dbfebb51
Kaushiki Saxena <kaushiki1507@gmail.com>
2026-06-08 12:14:36 +0530

    Improve duplicate detection in hasDuplicate function
    Refactor duplicate detection logic for clarity.

--- Data Structures & Algorithms/duplicate-integer/submission-3.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -1,12 +1,15 @@
+//Time Complexity: O(n) Space Complexity: O(n)
 class Solution {
 public:
+//unordered set used to make a hash map
 unordered_set<int> seen;
     bool hasDuplicate(vector<int>& nums) {
         int n = nums.size();
         for(int i =0; i<n;i++){
+            //if number is found(seen.end() is when number isn't found, so if is not equal to number isn't found that means number is found)
         if(seen.find(nums[i]) != seen.end())
             return true;
-        else
+        else//if number isn't found insert it in the hash map
         seen.insert(nums[i]);
         }
            
