21427c4fe3ef2dd9dd1af37ce0ea8dc52fb4a431
Kaushiki Saxena <kaushiki1507@gmail.com>
2026-06-10 12:53:41 +0530

    Refactor isPalindrome function for clarity
    Refactor isPalindrome function to build strings a and b from alphanumeric characters in both forward and backward directions.

--- Data Structures & Algorithms/is-palindrome/submission-0.cpp
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -1,25 +1,27 @@
+//Time Complexity:O(n) Space Complexity:O(1)
 class Solution {
 public:
     bool isPalindrome(string s) {
         int n = s.size();
+        //initializing strings as spaces
         string a = ""; 
         string b = "";
-        for (int i =0;i<n;i++)
+        for (int i =0;i<n;i++)//forward
         {
-            if(isalnum(s[i]) != 0){
+            if(isalnum(s[i]) != 0){//if a number or alphabet, append to a
             a += tolower(s[i]);
             }
         }
-        for (int i =n-1;i>=0;i--)
+        for (int i =n-1;i>=0;i--)//backward
         {
-            if(isalnum(s[i]) != 0){
+            if(isalnum(s[i]) != 0){//if a number or alphabet append to b
             b +=tolower(s[i]);
             }
         }        
-        if(a==b)
+        if(a==b)//if strings equal return true
         {
             return true;
         }
        
-  return false;  }
+  return false;  }//else return false
 };
